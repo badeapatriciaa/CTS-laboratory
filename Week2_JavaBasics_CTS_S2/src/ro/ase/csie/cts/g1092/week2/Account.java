84def2c7de5623d6aa3dad586facd739d27c9540
Patricia Badea <badeapatricia18@stud.ase.ro>
2021-03-02 11:29:35 +0200

    final commit

--- Week2_JavaBasics_CTS_S2/src/ro/ase/csie/cts/g1092/week2/Account.java
+++ Week2_JavaBasics_CTS_S2/src/ro/ase/csie/cts/g1092/week2/Account.java
@@ -1,11 +1,14 @@
 package ro.ase.csie.cts.g1092.week2;
 
+import ro.ase.csie.cts.g1092.week2.exceptions.IllegalTransferException;
+import ro.ase.csie.cts.g1092.week2.exceptions.InsufficientFundsException;
+
 public abstract class Account {
 
 	public abstract double getBalance();
 	public abstract void deposit(double amount);//daca voiam sa pun orice acolo, foloseam Object in loc de double
-	public abstract void transfer(Account destination, double amount);
-	public abstract void withdraw(double amount);
+	public abstract void transfer(Account destination, double amount) throws InsufficientFundsException, IllegalTransferException;
+	public abstract void withdraw(double amount) throws InsufficientFundsException;
 	
 	}
 
