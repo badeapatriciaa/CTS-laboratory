005a268da95b26d895156048a6b726f6019d1572
Patricia Badea <badeapatricia18@stud.ase.ro>
2021-03-11 22:37:47 +0200

    final commit

--- Week3_Refactor_Sales_Stragety/src/ro/ase/cts/g1092/refactor/phase3/SalesStrategy.java
+++ Week3_Refactor_Sales_Stragety/src/ro/ase/cts/g1092/refactor/phase3/SalesStrategy.java
@@ -4,47 +4,44 @@ import ro.ase.cts.g1092.refactor.exceptions.InvalidValueException;
 
 public class SalesStrategy {
 //camelcase 
-	 public float computeFinalPrice(int productType, float initialPrice, int yearsSinceRegistration) throws InvalidValueException
-	    {
-		 
-		 public static final int FIDELITY_YEARS_THRESHOLD = 10;
-		    public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
+	 public static final int FIDELITY_YEARS_THRESHOLD = 10;
+	    public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
 
-		    public static float getFidelityDiscount(int yearsSinceRegistration) {
-		        return (yearsSinceRegistration > FIDELITY_YEARS_THRESHOLD) ? MAX_FIDELITY_DISCOUNT : (float)yearsSinceRegistration/100;
-		    }
+	    public static float getFidelityDiscount(int yearsSinceRegistration) {
+	        return (yearsSinceRegistration > FIDELITY_YEARS_THRESHOLD) ? MAX_FIDELITY_DISCOUNT : (float)yearsSinceRegistration/100;
+	    }
 
-		    public static float getPriceWithDiscount(float initialPrice,float discount, float fidelityDiscount) {
-		        return (initialPrice - (discount * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice));
-		    }
+	    public static float getPriceWithDiscount(float initialPrice,float discount, float fidelityDiscount) {
+	        return (initialPrice - (discount * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice));
+	    }
 
-		    public float computeFinalPrice(ProductType productType, float initialPrice, int yearsSinceRegistration) throws InvalidValueException {
+	    public float computeFinalPrice(ProductType productType, float initialPrice, int yearsSinceRegistration) throws InvalidValueException {
 
-		        if(initialPrice <= 0 || yearsSinceRegistration < 0) {
-		            throw new InvalidValueException();
-		        }
-		        
-		        float finalPrice;
-		        float fidelityDiscount = getFidelityDiscount(yearsSinceRegistration);
+	        if(initialPrice <= 0  || yearsSinceRegistration < 0) {
+	            throw new InvalidValueException();
+	        }
+	        
+	        float finalPrice;
+	        float fidelityDiscount = getFidelityDiscount(yearsSinceRegistration);
 
-		        switch (productType) {
-		            case NEW:
-		                finalPrice = initialPrice;
-		                break;
-		            case DISCOUNTED:
-		                finalPrice = getPriceWithDiscount(initialPrice,ProductType.DISCOUNTED.getDiscount(),fidelityDiscount);
-		                break;
-		            case LIMITED_STOCK:
-		                finalPrice = getPriceWithDiscount(initialPrice,ProductType.LIMITED_STOCK.getDiscount(),fidelityDiscount);
-		                break;
-		            case LEGACY:
-		                finalPrice = getPriceWithDiscount(initialPrice,ProductType.LEGACY.getDiscount(),fidelityDiscount);
-		                break;
-		            default:
-		                throw new UnsupportedOperationException("Product type not managed");
-		        }
+	        switch (productType) {
+	            case NEW:
+	                finalPrice = initialPrice;
+	                break;
+	            case DISCOUNTED:
+	                finalPrice = getPriceWithDiscount(initialPrice,ProductType.DISCOUNTED.getDiscount(),fidelityDiscount);
+	                break;
+	            case LIMITED_STOCK:
+	                finalPrice = getPriceWithDiscount(initialPrice,ProductType.LIMITED_STOCK.getDiscount(),fidelityDiscount);
+	                break;
+	            case LEGACY:
+	                finalPrice = getPriceWithDiscount(initialPrice,ProductType.LEGACY.getDiscount(),fidelityDiscount);
+	                break;
+	            default:
+	                throw new UnsupportedOperationException("Product type not managed");
+	        }
 
-		        return finalPrice;
-		    }
+	        return finalPrice;
+	    }
 
 }
